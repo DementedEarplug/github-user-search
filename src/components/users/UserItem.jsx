bd2d431f3225832551be69f923a61b37b33ed459
Onix <gotnix13@gmail.com>
2021-02-26 11:45:59 -0500

    Converted statless components from class component to function components. This was the standard in the old days.

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,37 +1,30 @@
-import React, { Component } from "react";
-
-export class UserItem extends Component {
-  // //Adding state
-  // state = {
-  //   id: 1,
-  //   login: "mojombo",
-  //   avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-  //   html_url: "https://github.com/mojombo",
-  // };
+import React from "react";
+import PropTypes from 'prop-types'
 
 
-
-  render() {
-    //De-structure this.state
-    // const { login, avatar_url, html_url } = this.state;
-
-    return (
-      <div className='card text-center'>
-        <img
-          src={this.props.avatar}
-          alt=''
-          className='round-img'
-          style={{ width: "60px" }}
-        />
-        <h3>{this.props.login}</h3>
-        <div>
-          <a href={this.props.url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+const UserItem = (props) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={props.avatar}
+        alt=''
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{props.login}</h3>
+      <div>
+        <a href={props.url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+UserItem.PropTypes = {
+  avatar: PropTypes.string.isRequired,
+  login: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
 
 export default UserItem;
