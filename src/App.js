2dc625b8f6772d40a2f0e6dfd39e43087b7aafc0
Onix <gotnix13@gmail.com>
2021-02-26 10:32:29 -0500

    Implemented default props, and proptypes to make app more robust. Added Fonstawesome cnd to bring in icons. Added navbar component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Navbar } from "./components/layout/Navbar";
+
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Hello Boiiiiiiiiizee</h1>
       </div>
     );
