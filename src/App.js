92f38155db2ae17779a883ff3e85bf895d3c545d
Onix <gotnix13@gmail.com>
2021-03-31 16:00:15 -0400

    Added react router to the app and an about page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+
 import axios from "axios";
 
 import "./App.css";
@@ -37,27 +41,41 @@ class App extends Component {
   };
 
   setAlert = (message, type) => {
-    this.setState({ alert: {  message, type } });
-    setTimeout(()=>
-      this.setState({alert:null}), 3000
-    )
+    this.setState({ alert: { message, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
   };
 
   render() {
     return (
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={this.state.alert}></Alert>
-          <Search
-            searchUsers={this.searchUsers}
-            clearSearch={this.clearSearch}
-            showClear={this.state.users.length > 0}
-            setAlert={this.setAlert}
-          />
-          <Users users={this.state.users} loading={this.state.loading} />
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert}></Alert>
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearSearch={this.clearSearch}
+                      showClear={this.state.users.length > 0}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      users={this.state.users}
+                      loading={this.state.loading}
+                    />
+                  </Fragment>
+                )}
+              ></Route>
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p>This app uses the github api to search users accounts on github and redirects to their profile and information so you can check out their projects.</p>
+      <p>v0.0.0</p>
+    </div>
+  )
+}
+
+export default About
