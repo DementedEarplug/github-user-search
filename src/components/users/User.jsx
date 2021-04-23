eafaabc9a0fedee0176611e1ebc5e573b9919c98
Onix <gotnix13@gmail.com>
2021-04-22 23:31:51 -0400

    Populating and styling user profile.

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,7 +1,16 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 import Loading from "../layout/Loading";
 
 class User extends Component {
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
+
   componentDidMount() {
     //* This uses the URL param :login from UserItem and calls the function passed down as a prop passing :login as an argument.
     this.props.getUser(this.props.match.params.login);
@@ -10,18 +19,90 @@ class User extends Component {
   render() {
     const {
       name,
-      // avatar_url,
-      // bio,
-      // blog,
+      avatar_url,
+      bio,
+      blog,
       login,
-      // html_url,
-      // followers,
-      // public_repos,
-      // public_gists,
-      // hireable
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      company,
+      location,
+      hireable,
     } = this.props.user;
 
-    return(this.props.loading ? <Loading /> : <div>{login}</div>);
+    return this.props.loading ? (
+      <Loading />
+    ) : (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+        Hirable:{" "}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt=''
+              className='round-img'
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>{location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong>
+                    {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong>
+                    {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong>
+                    {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-danger'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
